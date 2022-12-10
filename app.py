7ba2a5202f9bd2861a17a3ac06d378e6c776e307
rainfidelis <rainnyfidelis@yahoo.com>
2022-12-10 12:48:56 +0100

    improve website aesthetic

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+"""
+This script runs the FlaskTemplate application using a development server.
+"""
 
+from os import environ
+from FlaskTemplate import app
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '3000'))
+    except ValueError:
+        PORT = 3000
+    app.run(HOST, PORT)
